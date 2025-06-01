0ac467826680c760601fa5bbf64f5c8bf46ed9a3
Roberto Javier Lagger <javierlaggerr@gmail.com>
2025-06-01 08:00:37 -0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
   return (
     <div className="mx-auto mb-4 flex items-center justify-center">
       <img 
-        src="images/Logo.jpeg" 
+        src="/images/Logo.jpeg" 
         alt="Restaurante Logo" 
         className="w-24 h-24 md:w-28 md:h-28 object-contain" 
       />
